46c3c47a6e375dbf25b5a818ffcfec910b3314ee
Matthew Gibb <87804850+matthewg543@users.noreply.github.com>
2022-02-03 19:20:16 +0000

    initial booking setup

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,28 @@
+class BookingsController < ApplicationController
+  def index
+    bookings = Bookings.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    #fetches an empty form
+    @booking = Booking.new
+  end
+
+  def create
+  end 
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+end
+
+#params id for create method. Tree controller
+#def booking_params
+    #params.require(:restaurant).permit(:name, :rating, :address)
+  end
+#end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_162354) do
+ActiveRecord::Schema.define(version: 2022_02_01_191135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date", precision: 6
+    t.datetime "end_date", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
