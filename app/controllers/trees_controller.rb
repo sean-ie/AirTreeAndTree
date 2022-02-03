c92ec103d26b5007697a0510e7495cc46993024c
lorraine chambers <lorrainedeniece@gmail.com>
2022-02-03 20:26:43 +0000

    trees model/controller

---
+++ app/controllers/trees_controller.rb
@@ -0,0 +1,42 @@
+class TreesController < ApplicationController
+
+  before_action :find_tree, only [:show, :edit, :update, :destroy]
+
+  def index
+    @trees = Tree.all
+  end
+
+  def new
+    @tree = Tree.new
+  end
+
+  def create
+    @tree = Tree.new(tree_params)
+    @tree.user = current_user
+    if @tree.save
+      redirect_to trees_path
+   else
+     render :new
+   end
+  end
+
+  def show
+  end
+
+  def update
+    @tree.update(tree_params)
+  end
+
+  def destroy
+    @tree.destroy
+  end
+
+  private
+
+  def tree_params
+    params.require(:tree).permit(:name, :description, :price, :address, :number_of_guests)
+  end
+
+  def find_tree
+    @tree = Tree.find(params[:id])
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating user..'
+lc = User.create(email: "lc@me.com", password: 1234567) 
+lc.save!
+puts 'Clearing database..'
+Tree.destroy_all
+
+puts 'Creating Treehouses'
+
+tree = Tree.create(name: "Birch Treehouse", description: "Cosy heights", address: "123 Birchwood Forest", price: 50.00 , number_of_guests: 4 , type_of_tree: "Birch", user_id: lc.id)
+tree.save!
+# Tree.create(name: "Oak Treehouse", description: "Lofty Oak", address: "528 OakWood Heights", price: 40.00 , number_of_guests: 3 , type_of_tree: "Oak", user_id: User.first)
+# Tree.create(name: "RainForest Treehouse", description: "Rainforest Home", address: "267 RainForest way", price: 90.00 , number_of_guests: 5 , type_of_tree: "Kapok", user_id: User.first)
+
+puts 'Treehouses created'
